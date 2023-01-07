54f678eed3aad046b8cb081e0d0bb69647333461
Jozef Izso <jozef.izso@gmail.com>
2023-01-07 20:15:07 +0100

    Improve navigation

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -7,7 +7,6 @@ export default defineConfigWithTheme<ThemeConfig>({
   extends: baseConfig,
 
   srcDir: 'app',
-  cleanUrls: 'with-subfolders',
 
   title: 'NetOffice',
   description: 'NetOffice Framework documentation.',
@@ -15,21 +14,45 @@ export default defineConfigWithTheme<ThemeConfig>({
 
   themeConfig: {
     nav: [
-      { text: 'Office Add-ins', link: '/addins/'},
-      { text: 'Tutorials', link: '/tutorials/'},
-      { text: 'Toolbox', link: '/toolbox/'},
-      { text: 'Documentation', link: '/documentation/'}
-    ],
-
-    sidebar: [
       {
-        text: 'Guide',
-        items: [
-          { text: 'Example', link: '/example' },
-          // ...
-        ],
+        text: 'Office Add-in',
+        link: '/addins/',
+        activeMatch: '^/addins/'
+      },
+      {
+        text: 'Tutorials',
+        link: '/tutorials/',
+        activeMatch: '^/tutorials/'
+      },
+      {
+        text: 'Toolbox',
+        link: '/toolbox/',
+        activeMatch: '^/toolbox/'
+      },
+      {
+        text: 'Documentation',
+        link: '/documentation/',
+        activeMatch: '^/documentation/'
       },
     ],
+
+    sidebar: {
+      '/tutorials/': [
+        {
+          text: 'NetOffice Tutorials',
+          items: [
+            { text: 'Understand COM Proxy Management', link: '/tutorials/tutorial01_en_cs' },
+            { text: 'Efficient using Dispose', link: '/tutorials/tutorial02_en_cs' },
+            { text: 'Using Dispose with event exporting Objects', link: '/tutorials/tutorial03_en_cs' },
+            { text: 'Observable COM Proxy Count', link: '/tutorials/tutorial04_en_cs' },
+            { text: 'Understanding COMObject', link: '/tutorials/tutorial05_en_cs' },
+            { text: 'Understand Variant Type', link: '/tutorials/tutorial06_en_cs' },
+            { text: 'Invoker', link: '/tutorials/tutorial07_en_cs' },
+            { text: 'Check for a specific entity support at runtime', link: '/tutorials/tutorial08_en_cs' },
+          ],
+        }
+      ]
+    },
 
     socialLinks: [
       { icon: 'github', link: 'https://github.com/NetOfficeFw/NetOffice' },
